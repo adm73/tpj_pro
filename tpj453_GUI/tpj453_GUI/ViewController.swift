e673590042c5f767960cbdd12453e1dfd1380c98
adm73 <chenadm73@gmail.com>
2016-09-21 14:11:11 -0400

    1.1

---
+++ tpj453_GUI/tpj453_GUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tpj453_GUI
+//
+//  Created by ChenYu on 2016-09-21.
+//  Copyright © 2016 chyapp.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
